b2166e7b6ecb0a2cdc0cc916f02d4caf73bdb48a
Gizem <gizbglm@gmail.com>
2026-08-14 23:28:26 +0300

    api updates

---
+++ Core_Project/Areas/Writer/Controllers/DashboardController.cs
@@ -0,0 +1,39 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace Core_Project.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.o = values.Name + " " + values.Surname;
+
+            //Weather APi
+            //string api = "f5d9f7cbcbf4dcb16e066870d5dd5884";
+            //string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
+            //XDocument document = XDocument.Load(connection);
+            //ViewBag.v5 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+            //statistics
+            Context c = new Context();
+            ViewBag.v1 = 0;
+            ViewBag.v2 = c.Announcements.Count();
+            ViewBag.v3 = c.Users.Count();
+            ViewBag.v4 = c.Skills.Count();
+            return View();
+        }
+    }
+}
