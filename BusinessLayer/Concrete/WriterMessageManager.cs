11af768d8989828153ecdcedaaa5397e6739fb03
Gizem <gizbglm@gmail.com>
2026-08-16 23:26:44 +0300

    Message Started

---
+++ BusinessLayer/Concrete/WriterMessageManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Concrete
+{
+    public class WriterMessageManager : IWriterMessageService
+    {
+        IWriterMessageDal _writerMessageDal;
+
+        public WriterMessageManager(IWriterMessageDal writerMessageDal)
+        {
+            _writerMessageDal = writerMessageDal;
+        }
+
+        public void TAdd(WriterMessage t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(WriterMessage t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WriterMessage TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<WriterMessage> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(WriterMessage t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
