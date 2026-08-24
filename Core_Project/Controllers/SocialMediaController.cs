d591e538f365eeee32871f9135ce9b7b4ba744c8
Gizem <gizbglm@gmail.com>
2026-08-24 23:54:12 +0300

    testimonials updates

---
+++ Core_Project/Controllers/SocialMediaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Core_Project/Controllers/TestimonialController.cs
@@ -0,0 +1,60 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Entity_Framework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class TestimonialController : Controller
+    {
+
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
+
+        public IActionResult Index()
+        {
+            var values = testimonialManager.TGetList();
+            return View(values);
+        }
+
+
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var values = testimonialManager.TGetByID(id);
+            testimonialManager.TDelete(values);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpGet]
+        public IActionResult EditTestimonial(int id)
+        {
+            var values = testimonialManager.TGetByID(id);
+            return View(values);
+        }
+
+
+        [HttpPost]
+        public IActionResult EditTestimonial(Testimonial testimonial)
+        {
+            testimonialManager.TUpdate(testimonial);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+        [HttpGet]
+        public IActionResult AddTestimonial()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult AddTestimonial(Testimonial testimonial)
+        {
+            testimonialManager.TAdd(testimonial);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
