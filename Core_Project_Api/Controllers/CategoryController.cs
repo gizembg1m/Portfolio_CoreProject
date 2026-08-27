cd1b566f0340da5c4b5a8ff80c04d80ab47a3c77
Gizem <gizbglm@gmail.com>
2026-08-28 01:40:41 +0300

    api started

---
+++ Core_Project_Api/Controllers/CategoryController.cs
@@ -0,0 +1,75 @@
+﻿using Core_Project_Api.DAL.ApiContext;
+using Core_Project_Api.DAL.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public Context context = new Context();
+
+
+        [HttpGet]
+        public IActionResult GetCagegoryList()
+        {
+            return Ok(context.Categories.ToList());
+        }
+
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetID(int id)
+        {
+            var value = context.Categories.Find(id);
+
+            if (value.Equals(null))
+                return NotFound();
+            return Ok(value);
+
+        }
+
+
+        [HttpPost]
+        public IActionResult CategoryAdd(Category p)
+        {
+            context.Add(p);
+            context.SaveChanges();
+            return Created("", p);
+        }
+
+
+
+        [HttpDelete]
+        public IActionResult CategoryDelete(int id)
+        {
+            var value = context.Categories.Find(id);
+            if (value.Equals(null))
+                return NotFound();
+            else
+            {
+                context.Remove(value);
+                context.SaveChanges();
+                return NoContent();
+            }
+        }
+
+
+
+        [HttpPut]
+        public IActionResult UpdateCategory(Category p)
+        {
+            var value = context.Find<Category>(p.CategoryID);
+            if (value.Equals(null))
+                return NotFound();
+            else
+            {
+                value.CategoryName = p.CategoryName;
+                context.Update(value);
+                context.SaveChanges();
+                return Ok(value);
+            }
+        }
+    }
+}
---
+++ Core_Project_Api/DAL/ApiContext/Context.cs
@@ -0,0 +1,14 @@
+﻿using Core_Project_Api.DAL.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core_Project_Api.DAL.ApiContext
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost; Database=CoreProjectDB2; Integrated Security=True; TrustServerCertificate=True;");
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
