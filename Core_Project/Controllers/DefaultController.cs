0b75e621b342351b723fb5874557a55d2a55c63d
Gizem <gizbglm@gmail.com>
2026-08-29 14:57:17 +0300

    last updates

--- Core_Project/Controllers/DefaultController.cs
+++ Core_Project/Controllers/DefaultController.cs
@@ -1,10 +1,12 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Entity_Framework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Project.Controllers
 {
+    [AllowAnonymous]
     public class DefaultController : Controller
     {
         public IActionResult Index()
